43f876dceea4cc4889f3ef33cd783b8356fcc117
weinh <weinh@hundsun.com>
2018-05-30 17:13:11 +0800

    垃圾收集算法

--- src/main/java/jvm/gc/FinalizeGC.java
+++ src/main/java/jvm/gc/FinalizeGC.java
@@ -1,10 +1,45 @@
 package jvm.gc;
 
 /**
- * 类 名 称：FinalizeGC.java
- * 功能说明：
- * 开发人员：weinh
- * 开发时间：2018年04月24日
+ * <p>类 名 称：FinalizeGC.java</p>
+ * <p>功能说明：</p>
+ * <p>开发时间：2018年04月24日</p>
+ *
+ * @author ：weinh
  */
 public class FinalizeGC {
+    private static FinalizeGC SAVA_HOOK = null;
+
+    public void isAlive() {
+        System.out.println("我还活着！");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize方法执行，逃过一劫");
+        FinalizeGC.SAVA_HOOK = this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SAVA_HOOK = new FinalizeGC();
+
+        SAVA_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if (SAVA_HOOK != null) {
+            SAVA_HOOK.isAlive();
+        } else {
+            System.out.println("终于死了！");
+        }
+
+        SAVA_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if (SAVA_HOOK != null) {
+            SAVA_HOOK.isAlive();
+        } else {
+            System.out.println("终于死了！");
+        }
+    }
 }
