f4cb25674edd0e5eb8cc5c8e1f6d67a2d0a1b282
weinh <weinh@hundsun.com>
2018-07-27 14:04:13 +0800

    jstack介绍

--- src/main/java/jvm/jstack/JstackOut.java
+++ src/main/java/jvm/jstack/JstackOut.java
@@ -18,54 +18,109 @@ public class JstackOut {
     Object a = new Object();
     Object b = new Object();
 
-    public static void main(String[] args) {
-//        singleThread();
-//        threadPool();
-        new JstackOut().deadlock();
-        while (true) ;
-    }
+    Lock lock = new ReentrantLock();
 
-    private static void singleThread() {
-        Thread singleThread = new Thread(() -> {
-            while (true) {
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        singleThread.setName("singleThread");
-        singleThread.start();
+    public static void main(String[] args) throws InterruptedException {
+        threadPool();
+        new JstackOut().deadlock();
+        new JstackOut().waitingLock1();
+        new JstackOut().waitingLock2();
+        new JstackOut().waiting();
+        while (true) ;
     }
 
     private static void threadPool() {
         Executor executor = Executors.newFixedThreadPool(4);
+        executor.execute(() -> Thread.currentThread().setName("thread pool 1"));
         executor.execute(() -> {
-            Thread.currentThread().setName("thread pool 1");
-            while (true) {
+        });
+    }
+
+    private void waiting() throws InterruptedException {
+        new Thread(() -> {
+            Thread.currentThread().setName("sleep thread");
+            try {
+                Thread.sleep(60 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            Thread.currentThread().setName("wait thread");
+            synchronized (a) {
                 try {
-                    Thread.sleep(10000);
+                    a.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-        });
-        executor.execute(() -> Thread.currentThread().setName("thread pool 2"));
-    }
+        }).start();
 
-    private void deadlock() {
-        for (int i = 0; i < 10; i++) {
-
-            Thread threadA = new Thread(() -> {
-                Thread.currentThread().setName("thread a");
+        new Thread(() -> {
+            Thread.currentThread().setName("wait time thread");
+            synchronized (a) {
                 try {
-                    Thread.sleep(500);
+                    a.wait(60 * 1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+        }).start();
+
+        Thread.currentThread().join(10 * 1000);
+        Thread.currentThread().join();
+    }
+
+    private void waitingLock1() throws InterruptedException {
+        new Thread(() -> {
+            Thread.currentThread().setName("lock thread1");
+            lock.lock();
+            while (true) ;
+        }).start();
+
+        Thread.sleep(500);
+
+        new Thread(() -> {
+            Thread.currentThread().setName("lock thread2");
+            lock.lock();
+        }).start();
+    }
+
+    private void waitingLock2() throws InterruptedException {
+        new Thread(() -> {
+            Thread.currentThread().setName("synchronized thread1");
+            synchronized (a) {
+                while (true) ;
+            }
+        }).start();
+
+        Thread.sleep(500);
+
+        new Thread(() -> {
+            Thread.currentThread().setName("synchronized thread2");
+            synchronized (a) {
+            }
+        }).start();
+    }
+
+    private void deadlock() {
+        for (int i = 0; i < 1; i++) {
+
+            Thread threadA = new Thread(() -> {
+                Thread.currentThread().setName("thread a");
                 synchronized (a) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     synchronized (b) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
 
@@ -73,13 +128,18 @@ public class JstackOut {
 
             Thread threadB = new Thread(() -> {
                 Thread.currentThread().setName("thread b");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 synchronized (b) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     synchronized (a) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             });
